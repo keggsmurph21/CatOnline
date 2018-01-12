519b0390927ea314052c7448077548f256ecd8b6
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-01-12 18:30:18 -0500

    fork of chat messenger using socket.io / express on Node.js

---
+++ index.js
@@ -0,0 +1,71 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function() {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0
+
+io.sockets.on('connection', function(socket) {
+
+ console.log('a new client connected');
+ var addedUser = false;
+
+ socket.on('new message', function(data) {
+   socket.broadcast.emit('new message', {
+     username: socket.username,
+     message: data
+   });
+ });
+
+  socket.on('add user', function(username) {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+
+    socket.broadcast.emit('user joined', {
+        username: socket.username,
+        numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', function() {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', function() {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', function() {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+
+});
