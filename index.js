a458bf7aff21f6b15acb6cc287e2f6af3400347f
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-01-12 19:16:23 -0500

    db testing

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ server.listen(port, function() {
   console.log('Server listening at port %d', port);
 });
 
+// Setup postgreSQL DB
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
