5f2080c22f644c35dd134dbe84472a7625cef778
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-02-20 01:38:59 -0500

    tweaking script a bit

--- config/game-states/convert-tsv-to-json.py
+++ config/game-states/convert-tsv-to-json.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# data for this .tsv file lives at https://goo.gl/c7dDXv
+# expect data in this format:
+# id | source | target | conditions | priority | multi | cancel | label
+
 import csv, json, sys, os
 
 def main():
@@ -12,30 +16,32 @@ def main():
         print( 'ERROR file does not exist' )
         return
 
-    JSON = {}
+    data = {}
     isContent = False
     with open(tsvfile, 'r') as f:
         tsv = csv.reader(f, delimiter='\t')
         for row in tsv:
             if isContent: # skip the header row
-                while len(row) < 8:
+                while len(row) < 9:
                     row.append('')
-                data = {
+                if row[2] not in data:
+                    data[row[2]] = {}
+
+                data[row[2]][row[1]] = {
                     "name":row[0],
                     "target":row[3],
                     "conditions":row[4],
                     "isPriority":len(row[5])>0,
                     "isMulti":len(row[6])>0,
-                    "isCancel":len(row[7])>0 }
+                    "isCancel":len(row[7])>0,
+                    "label":row[8]
+                }
 
-                if row[2] not in JSON:
-                    JSON[row[2]] = {}
-                JSON[row[2]][row[1]] = data
             isContent = True
 
     jsonfile = sys.argv[2] if len(sys.argv)>2 else sys.argv[1].replace('tsv','json')
     with open(jsonfile, 'w') as f:
-        json.dump( JSON, f, indent=3 )
+        json.dump( data, f, indent=3 )
 
 if __name__ == '__main__':
     main();
