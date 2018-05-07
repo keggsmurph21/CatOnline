53ba112bde7cd90955c1a5336529ab4b194cdbb0
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-06 23:05:30 -0300

    install script changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-git clone https://github.com/keggsmurph21/catonline
+if [ ! -d catonline ]; then
+  git clone https://github.com/keggsmurph21/catonline
+fi
 cd catonline
 git checkout module
 git submodule update --recursive --remote
 cd src/catext
+ls
+return
 pip install -r requirements.txt
 cd ../..
 
